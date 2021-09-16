2e6482d4dd6c78887d64a365769c32a0cfca8aee
Dio Riandika <dio.rndk@gmail.com>
2021-09-16 18:41:24 +0700

    - add logo - add home screen (on progress) - add some libraries

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import SafeViewAndroid from "../components/SafeViewAndroid";
+import tw from "tailwind-react-native-classnames"
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-            <Text>Home</Text>
+        <SafeAreaView style={SafeViewAndroid.AndroidSafeArea, tw `bg-white h-full`}>
+            <View style={tw `p-5`}>
+                <Image 
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain",
+                    }}
+                    // get image from local
+                    source={require('../assets/image/uber.png')}
+                    // get image from URI
+                    // source={{
+                    //     uri: "https://links.papareact.com/gzs"
+                    // }}
+                />
+                <NavOptions />
+            </View>
         </SafeAreaView>
     )
 }
